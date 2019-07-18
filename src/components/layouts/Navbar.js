5b24cf5c9797611ff9e65550b91004ed9e068367
Takuya Hirata <takuya.hirata@dcmail.ca>
2019-07-17 20:09:32 -0400

    initial commit

---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+
+const Navbar = () => (
+  <nav className="navbar bg-primary">
+    <h1>
+      <FaGithub />
+      Github Finder
+    </h1>
+  </nav>
+);
+
+export default Navbar;
